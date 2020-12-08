5b13e48cfba0357f76b555c26033278f8145e3ef
sc30gsw <haidoushi875@gmail.com>
2020-12-09 08:07:52 +0900

    Revert "usersテーブルの作成"
    This reverts commit 445c406a9fffb12d360bacefbb8c434b4c6742bf.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 end
-
+=======
+end
+>>>>>>> Stashed changes
+=======
+end
+>>>>>>> Stashed changes
