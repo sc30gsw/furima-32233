c9c4d87d66de8c27dd561e7111ef767b83195a02
sc30gsw <haidoushi875@gmail.com>
2020-12-09 08:22:13 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :family_name
+    validates :first_name
+    validates :family_name_reading
+    validates :first_name_reading
+    validates :birth
+  end
 end
