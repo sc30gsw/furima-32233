3fdecefbddce2ee4e762a3f2caaf4582683d46b5
sc30gsw <haidoushi875@gmail.com>
2020-12-09 08:52:00 +0900

    正規表記の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :family_name
-    validates :first_name
-    validates :family_name_reading
-    validates :first_name_reading
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Family name Full-width characters" }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "First name Full-width characters" }
+    validates :family_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "Family name kana Full-width katakana characters" }
+    validates :first_name_reading, foramt: { with: /\A[ァ-ヶー－]+\z/, message: "First name kana Full-width katakana charactwers" }
     validates :birth
   end
 end
