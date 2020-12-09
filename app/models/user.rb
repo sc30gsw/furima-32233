938554933bb475e80f1fe8045e1ea7ac0c00ee30
sc30gsw <haidoushi875@gmail.com>
2020-12-09 16:10:48 +0900

    正規表現を変数化

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
+  name_validate = /\A[ぁ-んァ-ン一-龥]/
+  name_reading_validate = /\A[ァ-ヶー－]+\z/
+
   with_options presence: true do
     validates :nickname
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i, message: "is invalid. Input half-width characters" }
-    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters" }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters" }
-    validates :family_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters" }
-    validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters" }
+    validates :family_name, format: { with: name_validate, message: "is invalid. Input full-width characters" }
+    validates :first_name, format: { with: name_validate, message: "is invalid. Input full-width characters" }
+    validates :family_name_reading, format: { with: name_reading_validate, message: "is invalid. Input full-width katakana characters" }
+    validates :first_name_reading, format: { with: name_reading_validate, message: "is invalid. Input full-width katakana characters" }
     validates :birth
   end
 end
