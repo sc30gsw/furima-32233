0ca715578eacdc6e10ea68970f6d64f925ae2ed4
sc30gsw <haidoushi875@gmail.com>
2020-12-09 11:52:16 +0900

    FactoryBotへuser情報の記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname {"test"}
+    email    {"test@test.com"}
+    password {"111aaa"}
+    password_confirmation {password}
+    family_name {"山田"}
+    first_name {"太郎"}
+    family_name_reading {"ヤマダ"}
+    first_name_reading {"タロウ"}
+    birth {"1990/1/1"}
   end
 end
