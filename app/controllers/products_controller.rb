13dc976e225fa2ba01026c362cfd3d35ad8b3cfd
sc30gsw <haidoushi875@gmail.com>
2020-12-10 07:22:54 +0900

    コントローラー名を変更

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,19 @@
-class ItemsController < ApplicationController
+class ProductsController < ApplicationController
+
   def index
   end
 
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new(product_params)
+  end
+
+  private
+  
+  def product_params
+    params.require(:product).permit(:naem, :content, :price).merge(user_id: current_user.id)
   end
 end
