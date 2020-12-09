9ea4525f18f5250d8cb314721ee2a9eee6208cd6
sc30gsw <haidoushi875@gmail.com>
2020-12-09 19:14:49 +0900

    マイグレーションファイルの修正

--- db/migrate/20201209090602_create_products.rb
+++ db/migrate/20201209090602_create_products.rb
@@ -3,6 +3,11 @@ class CreateProducts < ActiveRecord::Migration[6.0]
     create_table :products do |t|
       t.string     :name,            null: false
       t.text       :content,         null: false
+      t.integer    :category_id,     null: false
+      t.integer    :condition_id,    null: false
+      t.integer    :postage_type_id, null: false
+      t.integer    :prefecture_id,   null: false
+      t.integer    :delivery_day_id, null: false
       t.integer    :price,           null: false
       t.references :user,            null: false, foreign_key: true
       t.timestamps
