e02c85ea3580528b3a9d025f534f6d01c420030b
sc30gsw <haidoushi875@gmail.com>
2020-12-09 12:34:19 +0900

    Fakerの導入とFakerに合わせた記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    nickname {"test"}
-    email    {"test@test.com"}
-    password {"111aaa"}
+    nickname {Faker::Name.name}
+    email    {Faker::Internet.free_email}
+    password {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     family_name {"山田"}
     first_name {"太郎"}
