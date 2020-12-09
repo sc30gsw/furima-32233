2c8e2ae40dbb8942c9f46eef29c90c6015968192
sc30gsw <haidoushi875@gmail.com>
2020-12-10 07:01:20 +0900

    テーブルの編集

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,11 @@ ActiveRecord::Schema.define(version: 2020_12_09_090602) do
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "content", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "postage_type_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "delivery_day_id", null: false
     t.integer "price", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
