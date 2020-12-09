b1df37fef95be3f4cc6cc549e041f292f68f52b8
sc30gsw <haidoushi875@gmail.com>
2020-12-10 07:08:49 +0900

    バリデーションの作成

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :content
+    validates :category_id
+    validates :condition_id
+    validates :postage_type_id
+    validates :preefcture_id
+    validates :delivery_day_id
+    validates :price
+  end
 end
