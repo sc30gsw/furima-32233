9107efeef1a7cfdd9775294bd65a1fa7735b05bd
sc30gsw <haidoushi875@gmail.com>
2020-12-10 11:37:48 +0900

    アソシエーションの記述ミスを修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
-  belongs_to :pastage_type
+  belongs_to :postage_type
   belongs_to :prefecture
   belongs_to :delivery_day
   has_one_attached :image
