61dfc8ca5ca3c2bfe9b4606af12b8049763e4930
sc30gsw <haidoushi875@gmail.com>
2020-12-10 13:52:46 +0900

    販売手数料の計算の機能の実装

--- app/javascript/packs/product_price.js
+++ app/javascript/packs/product_price.js
@@ -2,7 +2,8 @@ function price (){
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
   });
 };
 window.addEventListener('load', price);
