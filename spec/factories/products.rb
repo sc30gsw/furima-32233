01e1bd48964cea275ef010e5f3fbffd0972b9eb6
sc30gsw <haidoushi875@gmail.com>
2020-12-10 14:11:29 +0900

    FactoryBotにprductの情報を記述

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :product do
-    
+    name {Faker::Name.name}
+    content {Faker::Loerm.sentence}
+    category_id {２}
+    condition_id {2}
+    postage_type_id {2}
+    prefecture_id {2}
+    delivery_day_id {2}
+    price {300}
+
+    association :user
+
+    after(:build) do |product|
+      product.image.attach(io: File.open('public/images/test_image.png'), filename: test_image.png)
+    end
   end
 end
