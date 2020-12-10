519664d2922227d3b4aa35255cf0096e8acea3d9
sc30gsw <haidoushi875@gmail.com>
2020-12-10 13:53:07 +0900

    販売利益計算機能の実装

--- app/javascript/packs/product_price.js
+++ app/javascript/packs/product_price.js
@@ -3,7 +3,9 @@ function price (){
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
     const addTaxDom = document.getElementById("add-tax-price");
+    const profit = document.getElementById("profit");
     addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+    profit.innerHTML = Math.floor(inputValue - inputValue * 0.1);
   });
 };
 window.addEventListener('load', price);
