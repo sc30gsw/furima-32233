c8caeab720feccf7d802c090145136cf61671d19
sc30gsw <haidoushi875@gmail.com>
2020-12-11 17:05:55 +0900

    削除処理の分岐を設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -40,6 +40,11 @@ class ProductsController < ApplicationController
   end
 
   def destroy
+    if product.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
   end
 
   private
