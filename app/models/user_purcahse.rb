a5def1d17f17ba09767cee0bfdb1c2009c80f08a
sc30gsw <haidoushi875@gmail.com>
2020-12-11 20:09:36 +0900

    adressの正規表現を除外

--- app/models/user_purcahse.rb
+++ app/models/user_purcahse.rb
@@ -6,7 +6,7 @@ class UserPurachse
     validates :postal_code, format: {with: /\A\d{3}[-]\d{4}\z/, message: 'Is invalid. Input half-width characters'}
     validates :prefecture_id
     validates :city, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: 'Is invalid. Input full-width characters'}
-    validates :address, format: {with: /[ -~｡-ﾟ]/, message: "Is invalid. Input full-width characters"}
+    validates :address
     validates :phone_number, format: {with: /\A\d{10,11}\z/, message: 'Is invalid, Do not need a hyphen'}
     validates :product_user
   end
