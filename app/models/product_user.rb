67b303bcd0a94190b584e9aecadde2b74d99fe11
sc30gsw <haidoushi875@gmail.com>
2020-12-12 11:43:20 +0900

    バリデーションの設定

--- app/models/product_user.rb
+++ app/models/product_user.rb
@@ -2,4 +2,9 @@ class ProductUser < ApplicationRecord
   belongs_to :product
   belongs_to :user
   has_one :purchase_info
+
+  with_options presence: true do
+    validates :product
+    validates :user
+  end
 end
