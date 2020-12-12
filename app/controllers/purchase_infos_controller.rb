b7dd795929d5412d40b752d39e4c91bfff6997ab
sc30gsw <haidoushi875@gmail.com>
2020-12-12 21:35:58 +0900

    環境変数の設定

--- app/controllers/purchase_infos_controller.rb
+++ app/controllers/purchase_infos_controller.rb
@@ -6,6 +6,7 @@ class PurchaseInfosController < ApplicationController
   def create
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
+      pay_item
       @purchase_address.save
       redirect_to root_path
     else
@@ -18,4 +19,13 @@ class PurchaseInfosController < ApplicationController
   def purchase_params
     params.permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, product_id: params[:product_id], token: params[:token])
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: purchase_params(product_id: params[:price]),
+        card: purchase_params[:token],
+        currency: 'jpy'
+      )
+    end
 end
---
+++ config/initializers/webpacker.rb
@@ -0,0 +1,1 @@
+Webpacker::Compailer.env["PAYJP_PUBLIC_KEY"] = ENV["PAYJP_PUBLIC_KEY"]
