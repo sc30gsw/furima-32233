457fb6db1aa348161eac270f9f8a26b43286c38f
sc30gsw <haidoushi875@gmail.com>
2020-12-12 12:24:17 +0900

    Formオブジェクトのインスタンス生成

--- app/controllers/purchase_infos_controller.rb
+++ app/controllers/purchase_infos_controller.rb
@@ -3,8 +3,21 @@ class PurchaseInfosController < ApplicationController
   end
 
   def new
+    @purcahse_info = PurchaseInfo.new
   end
 
   def create
+    @purchase_info = PurchaseInfo.new(purchase_params)
+    if @purcahse_info.valid?
+      @purcahse_info.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+
+  private
+
+  def purchase_params
   end
 end
