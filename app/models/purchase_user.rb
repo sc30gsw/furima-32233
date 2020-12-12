b9ffdd15e976fa828bf7aee1e13e25d68a6fce1a
sc30gsw <haidoushi875@gmail.com>
2020-12-12 10:51:36 +0900

    Formオブジェクトの記述の修正

--- app/models/purchase_user.rb
+++ app/models/purchase_user.rb
@@ -1,7 +1,7 @@
 class PurchaseUser
 
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :adress, :building_name, :phone_number, :product_user
+  attr_accessor :postal_code, :prefecture_id, :city, :adress, :building_name, :phone_number, :product_user, :product, :user
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A\d{3}[-]\d{4}\z/, message: 'Is invalid. Input half-width characters'}
@@ -10,8 +10,12 @@ class PurchaseUser
     validates :address # 番地については可能性が多いため正規表現を用いていない
     validates :phone_number, format: {with: /\A\d{10,11}\z/, message: 'Is invalid, Do not need a hyphen'}
     validates :product_user
+    validates :product
+    validates :user
   end
   
   def save
+    PurchaseInfo.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, adress: adress, building_name: building_name, phone_number: phone_number, product_user: product_user)
+    ProductUser.create(user: user, product: product)
   end
 end
