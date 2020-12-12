dcf3b95332344be60d7bd293c242c2f5920b0dac
sc30gsw <haidoushi875@gmail.com>
2020-12-12 16:31:08 +0900

    ルーティングからnewの除外

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'products#index'
   resources :products do
-    resources :purchase_infos, only: [:index, :new, :create]
+    resources :purchase_infos, only: [:index, :create]
   end
 end
