a6eb01fca4545c679ec6f36027d704e607bab318
sc30gsw <haidoushi875@gmail.com>
2020-12-12 09:47:39 +0900

    保存したいカラムの取得

--- app/models/purchase_user.rb
+++ app/models/purchase_user.rb
@@ -1,5 +1,5 @@
 class PurchaseUser
 
   include ActiveModel::Model
-
+  attr_accessor :nickname, :password, :family_name, :first_name, :family_name_reading, :first_name_reading, :birth, :image, :name, :content, :category_id, :condition_id, :postage_type_id, :delivery_day_id, :price, :postal_code, :prefecture_id, :city, :adress, :building_name, :phone_number, :product_user
 end
