2da7234d549bbe200f949d839c8543bd8ecb44bb
sc30gsw <haidoushi875@gmail.com>
2020-12-12 16:30:17 +0900

    Formオブジェクトのコントローラの処理記述

--- app/controllers/purchase_infos_controller.rb
+++ app/controllers/purchase_infos_controller.rb
@@ -1,24 +1,21 @@
 class PurchaseInfosController < ApplicationController
   def index
-  end
-
-  def new
-    @purcahse_adress = PurchaseAdress.new
+    @purchase_address = PurchaseAddress.new
   end
 
   def create
-    @purchase_adress = PurchaseAdress.new(purchase_params)
-    if @purcahse_adress.valid?
-      @purcahse_adress.save
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
       redirect_to root_path
     else
-      render action: :new
+      render action: :index
     end
   end
 
   private
 
   def purchase_params
-    params.require(:purchase_adress).permit(:postal_code, :prefecture_id, :city, :adress, :building_name, :phone_number).merge(user_id: current_user.id, product_id: params[:product_id])
+    params.permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, product_id: params[:product_id])
   end
 end
