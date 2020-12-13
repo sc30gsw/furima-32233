e88e05f2f3e5c27ab97416324d16433ddf3cd635
sc30gsw <haidoushi875@gmail.com>
2020-12-13 16:30:48 +0900

    テストコードFactoryBot記述

--- spec/factories/purchase_infos.rb
+++ spec/factories/purchase_infos.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :purchase_info do
-    
+    postal_code {"111-1111"}
+    prefecture_id {2}
+    city {"東京都"}
+    address {"2-2"}
+    phone_number {"09012345678"}
+    token {"tok_abcdefghijk00000000000000000"}
   end
 end
