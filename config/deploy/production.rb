cfb88ebf5ed54573d66f57aa1241c3926a066dd8
sc30gsw <haidoushi875@gmail.com>
2020-12-15 11:51:15 +0900

    記述ミスの修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '54.168.54.92', user: 'ec2-user,' roles: %w{app db web}
+server '54.168.54.92', user: 'ec2-user', roles: %w{app db web}
